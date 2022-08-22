adc2a694bb2644dc4893ce2d778b4ae984b8b410
Khaibar Saadat <khybersaadat93@gmail.com>
2022-08-22 21:53:47 +0200

    Complete javascript (state management of pigGame and it's logic)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,146 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PigGame from "./components/PigGame";
 
 import "./App.css";
 
-const player1Init = {
+let player1Init = {
   active: true,
-  score: 0,
   current: 0,
+  score: 0,
   won: false,
 };
 
-const player2Init = {
+let player2Init = {
   active: false,
-  score: 0,
   current: 0,
+  score: 0,
   won: false,
 };
 
 function App() {
+  const [showDice, setShowDice] = useState(false);
   const [player1, setPlayer1] = useState(player1Init);
   const [player2, setPlayer2] = useState(player2Init);
+  const [diceNumber, setDiceNumber] = useState(null);
+
+  console.log(player1);
+  console.log(player2);
+
+  useEffect(() => {
+    if (player1.score >= 100) {
+      setPlayer1({
+        ...player1,
+        won: true,
+      });
+    } else if (player2.score >= 100) {
+      setPlayer2({
+        ...player2,
+        won: true,
+      });
+    }
+  }, [player1.score, player2.score]);
+
+  // New game event handler
+  const handleRestart = () => {
+    setShowDice(false);
+    setPlayer1(player1Init);
+    setPlayer2(player2Init);
+  };
+
+  // Roll event handler
+  const handleRollDice = () => {
+    setShowDice(true);
+
+    // Generate random dice number from 1 to 6
+    const diceNumber = Math.floor(Math.random() * 6 + 1);
+    setDiceNumber(diceNumber);
+    console.log(diceNumber);
+
+    if (player1.active === true) {
+      if (diceNumber === 1) {
+        setPlayer1({
+          ...player1,
+          current: 0,
+          active: false,
+        });
+        setPlayer2({
+          ...player2,
+          active: true,
+        });
+      } else {
+        setPlayer1({
+          ...player1,
+          current: player1.current + diceNumber,
+        });
+      }
+    } else if (player2.active === true) {
+      if (diceNumber === 1) {
+        setPlayer2({
+          ...player2,
+          current: 0,
+          active: false,
+        });
+        setPlayer1({
+          ...player1,
+          active: true,
+        });
+      } else {
+        setPlayer2({
+          ...player2,
+          current: player2.current + diceNumber,
+        });
+      }
+    }
+  };
+
+  // Hold event handler
+  const handleHold = () => {
+    if (player1.active === true) {
+      setPlayer1({
+        ...player1,
+        score: player1.score + player1.current,
+        current: 0,
+        active: false,
+      });
+
+      setPlayer2({
+        ...player2,
+        active: true,
+      });
+    } else if (player2.active === true) {
+      setPlayer2({
+        ...player2,
+        score: player2.score + player2.current,
+        current: 0,
+        active: false,
+      });
+
+      setPlayer1({
+        ...player1,
+        active: true,
+      });
+    }
+  };
 
   return (
     <div className="App">
       <PigGame
-        onP1Active={player1.active}
-        onP1Score={player1.score}
-        onP1Current={player1.current}
-        onP2Active={player2.active}
-        onP2Score={player2.score}
-        onP2Current={player2.current}
+        // Player 1 states:
+        player1Active={player1.active}
+        player1Score={player1.score}
+        player1Current={player1.current}
+        player1Won={player1.won}
+        // Player 2 states:
+        player2Active={player2.active}
+        player2Score={player2.score}
+        player2Current={player2.current}
+        player2Won={player2.won}
+        // Events handlers and passed attributes:
+        onRestart={handleRestart}
+        onShowDice={showDice}
+        onRollDice={handleRollDice}
+        diceNumber={diceNumber}
+        onHold={handleHold}
       />
     </div>
   );
--- src/components/PigGame.js
+++ src/components/PigGame.js
@@ -1,35 +1,78 @@
 import dice1 from "../../src/assets/dice-1.png";
+import dice2 from "../../src/assets/dice-2.png";
+import dice3 from "../../src/assets/dice-3.png";
+import dice4 from "../../src/assets/dice-4.png";
+import dice5 from "../../src/assets/dice-5.png";
+import dice6 from "../../src/assets/dice-6.png";
 
 const PigGame = (props) => {
-  const player1Classes = props.onP1Active
+  const player1Classes = props.player1Active
     ? "player p1 active-player"
+    : props.player1Won
+    ? "player p1 won"
     : "player p1";
-  const player2Classes = props.onP2Active
+
+  const player2Classes = props.player2Active
     ? "player p2 active-player"
+    : props.player2Won
+    ? "player p2 won"
     : "player p2";
 
+  let dice = null;
+
+  switch (props.diceNumber) {
+    case 1:
+      dice = dice1;
+      break;
+    case 2:
+      dice = dice2;
+      break;
+    case 3:
+      dice = dice3;
+      break;
+    case 4:
+      dice = dice4;
+      break;
+    case 5:
+      dice = dice5;
+      break;
+    default:
+      dice = dice6;
+  }
   return (
     <div className="main-container">
       <div className={player1Classes}>
-        <h2 className="name">PLAYER 1</h2>
-        <p className="score">{props.onP1Score}</p>
+        <h2 className={props.player1Won ? "name victor-name-style" : "name"}>
+          PLAYER 1
+        </h2>
+        <p className="score">{props.player1Score}</p>
         <div className="current-container">
           <p className="current-label">CURRENT</p>
-          <p className="current-score">{props.onP1Current}</p>
+          <p className="current-score">{props.player1Current}</p>
         </div>
       </div>
       <div className="absolute-container">
-        <button className="new-game-btn">🔄 NEW GAME</button>
-        <img className="dice" src={dice1} alt="Dice" />
-        <button className="roll-dice-btn">🎲 ROLL DICE</button>
-        <button className="hold-btn">📥 HOLD</button>
+        <div className="absolute-within-container">
+          <button className="new-game-btn" onClick={props.onRestart}>
+            🔄 NEW GAME
+          </button>
+          {props.onShowDice && <img className="dice" src={dice} alt="Dice" />}
+        </div>
+        <button className="roll-dice-btn" onClick={props.onRollDice}>
+          🎲 ROLL DICE
+        </button>
+        <button className="hold-btn" onClick={props.onHold}>
+          📥 HOLD
+        </button>
       </div>
       <div className={player2Classes}>
-        <h2 className="name">PLAYER 2</h2>
-        <p className="score">{props.onP2Score}</p>
+        <h2 className={props.player2Won ? "name victor-name-style" : "name"}>
+          PLAYER 2
+        </h2>
+        <p className="score">{props.player2Score}</p>
         <div className="current-container ">
           <p className="current-label">CURRENT</p>
-          <p className="current-score">{props.onP2Current}</p>
+          <p className="current-score">{props.player2Current}</p>
         </div>
       </div>
     </div>
