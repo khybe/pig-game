1970f22f68378f106208da8297bb4de8d6c36f48
Khaibar Saadat <khybersaadat93@gmail.com>
2022-08-27 19:11:42 +0200

    Added modulated styles for Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
+import styles from "./Layout.module.css";
+
 const Layout = (props) => {
-  return <div className="main-container">{props.children}</div>;
+  return <div className={styles["main-container"]}>{props.children}</div>;
 };
 
 export default Layout;
